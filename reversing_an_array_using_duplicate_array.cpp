34657df4f67d432b194c06a7f9085095b4bb211c
ABDUL WARIS <gurkhooabdulwaris@gmail.com>
2025-01-06 16:16:08 +0530

    corrected file

--- reversing_an_array_using_duplicate_array.cpp
+++ reversing_an_array_using_duplicate_array.cpp
@@ -1,7 +1,7 @@
-/*******************************************reversing_an_array_using_duplicate_array********************************/
+/**************************to print reverse of a array using another array*******************/
 #include<iostream>
 using namespace std;
-void print(int arr[],int count)
+void print (int arr[],int count )
 {
     for (int i = 0; i < count; i++)
     {
@@ -9,40 +9,42 @@ void print(int arr[],int count)
     }
     
 }
-void duplicate(int arr[],int brr[],int size)
-{
-    int n=size-1;
-    for (int i = 0; i < size; i++)
+void duplicate (int arr[],int count)
     {
-        brr[i]=0;
-    }
-    
-    for (int i = 0; i < size; i++)
-    {
-        brr[n]=arr[i];
-        n--;
+        int brr[count];
+        int n=count-1;
+       
+        for (int i = 0; i < count; i++)
+        {
+            brr[n]=arr[i];
+            n--;
+        }
+       for(int i = 0; i < count ; i++)
+       {
+            arr[i]=brr[i];
+           
+       }
+             
 
     }
-    cout<<"reverse array is \n";
-        print(brr,size);
-}
 int main ()
     {
-         int size;
-        cout<<"enter the size of array \n";
+        int size;
+        cout<<"enter the size of array please";
         cin>>size;
         int arr[size];
-        cout<<"enter the elements of array";
+        cout<<"\n enter the elements of array please \n";
         for (int i = 0; i < size; i++)
         {
-            cout<<"eneter element "<<i+1<<" : ";
+            cout<<"enter element "<<i+1<<" : ";
             cin>>arr[i];
             cout<<endl;
-
         }
-        cout<<"the array you enter is \n";
-       print(arr,size);
-        cout<<endl;
-        int brr[size];
-        duplicate(arr,brr,size);
+        cout<<"\nthe array you entered is \n";
+        print(arr,size);
+        cout<<"\nthe reverse of the array is \n";
+        duplicate(arr,size);
+        print(arr,size);
+        return 0;
+        
     }
