58f5b7a7fd86f982419071f10398f1f708cab6cc
ABDUL WARIS <gurkhooabdulwaris@gmail.com>
2025-01-13 20:35:25 +0530

    merging logic added

---
+++ merging_two_array_using_another_array.cpp
@@ -0,0 +1,84 @@
+/**************************************merging_two_array_using_another_array**********************************/
+#include<iostream>
+using namespace std;
+void display(int arr[],int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            cout<<arr[i]<<"  ";
+        }
+        
+    }
+    void sort(int arr[],int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                for (int j = i+1; j < count; j++)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        swap(arr[i],arr[j]);
+                    }
+                }
+                
+            }
+            
+        }
+void merge(int arr1[],int size1,int arr2[],int size2)
+    {
+        int size=size1+size2;
+        int brr[size];
+        for (int i = 0; i < size; i++)
+        {
+            brr[i]=0;
+        } 
+       
+        
+        for (int i = 0; i < size1; i++)
+        {
+            brr[i]=arr1[i];
+        }
+       
+         for (int i = 0; i < size2; i++)
+        {
+           brr[size1+i]=arr2[i];
+        }
+       
+        sort(brr,size);
+        display(brr,size);
+
+        
+    }    
+
+int main ()
+    {
+        int size1,size2;
+        cout<<"enter the size of ist array\n";
+        cin>>size1;
+        int arr1[size1];
+        cout<<"\nenter the no. of elements of ist array \n";
+        for (int i = 0; i < size1; i++)
+        {
+            cout<<"\nenter the element no. "<<i+1<<" : ";
+            cin>>arr1[i];
+
+        }
+
+
+         cout<<"enter the size of 2nd array\n";
+        cin>>size2;
+        int arr2[size2];
+        cout<<"\nenter the no. of elements of 2nd array \n";
+        for (int i = 0; i < size2; i++)
+        {
+            cout<<"\nenter the element no. "<<i+1<<" : ";
+            cin>>arr2[i];
+
+        }
+       // sort(arr1,size1);
+      //  display(arr1,size1);
+       merge(arr1,size1,arr2,size2);
+       return 0;
+
+        
+    }
